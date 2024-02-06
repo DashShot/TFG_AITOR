c4d83b8be5788c7bc171c9abfaba10379f4bde5c
DashShot <susiloko33@gmail.com>
2024-02-06 21:12:37 +0100

    WebSocket Aplication V1

---
+++ webchat_agc/src/main/java/com/webchat_agc/webchat_agc/WebchatAgcApplication.java
@@ -0,0 +1,13 @@
+package com.webchat_agc.webchat_agc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebchatAgcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebchatAgcApplication.class, args);
+	}
+
+}
---
+++ webchat_agc/src/test/java/com/webchat_agc/webchat_agc/WebchatAgcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webchat_agc.webchat_agc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebchatAgcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
