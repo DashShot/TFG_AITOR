af3d6e68bd0eee97d137fac2818af2e0d4625325
root <root@DESKTOP-BDLT74Q>
2024-11-17 20:06:27 +0100

    cambios rutas

--- webchat_agc/src/main/java/com/webchat_agc/config/DocumentDBConf.java
+++ webchat_agc/src/main/java/com/webchat_agc/config/DocumentDBConf.java
@@ -12,6 +12,8 @@ public class DocumentDBConf {
 	
 	private MongoProperties properties;
 	
+	public static final String KEY_STORE_TYPE = "/tmp/certs/rds-truststore.jks";
+    	public static final String DEFAULT_KEY_STORE_PASSWORD = "amazonCApass";
 
         public DocumentDBConf(final MongoProperties properties) {
             super();
@@ -20,12 +22,18 @@ public class DocumentDBConf {
 
         @Bean
         public MongoClientSettings mongoClientSettings() { 
-            
+             setSslProperties();
 	     return MongoClientSettings.builder()
                     .applyToSslSettings(builder -> builder.enabled(true))
                     .build();
 	}
 
+        private static void setSslProperties() { 
+    	      System.setProperty("javax.net.ssl.trustStore", KEY_STORE_TYPE);
+    	      System.setProperty("javax.net.ssl.trustStorePassword",           
+                    DEFAULT_KEY_STORE_PASSWORD);
+        }
+	@SuppressWarnings({ "removal", "deprecation" })
     @Bean
         public MongoPropertiesClientSettingsBuilderCustomizer mongoPropertiesCustomizer(final MongoProperties properties) {
 			return new MongoPropertiesClientSettingsBuilderCustomizer(properties);
